88b44f5262aed70e7db6a448c3c96194128afbda
Taras <ytaras@gmail.com>
2017-07-17 11:09:55 +0300

    small fixes

--- src/Table.js
+++ src/Table.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Rating from 'react-rating';
-import {Table} from 'reactstrap';
+import orderBy from 'lodash/orderBy'
+import { Table } from 'reactstrap';
 
 const SortTypes = {
   ASC: 'ASC',
-  DESC: 'DESC',
+  DESC: 'DESC'
 };
 
 function reverseSortDirection(sortDir) {
@@ -16,9 +17,8 @@ class SortableTable extends React.Component {
   constructor() {
     super();
     this.state = {
-      nameSortDir: null,
-      ratingSortDir: null,
-      priceSortDir: null,
+      sortDir: {},
+      list: []
     };
     this.sortTable = this.sortTable.bind(this);
     this.onSortChange = this.onSortChange.bind(this);
@@ -30,57 +30,58 @@ class SortableTable extends React.Component {
     const sortDir = this.state.sortDir;
     this.setState({
       sortDir: {
-        [propertyName]: reverseSortDirection(sortDir[propertyName]),
-      },
+        [propertyName]: reverseSortDirection(sortDir[propertyName])
+      }
     });
   }
 
-  sortTable(sortDir, columnName) {
+  sortTable(sortDir = 'ASC', columnName) {
+    console.log('sortDir', sortDir, 'columnName', columnName);
+
+    this.setState({list: orderBy(this.props.price_list, [columnName], [sortDir.toLowerCase()])})
   }
 
   tableRender(list) {
-    return list.filter(elem => this.props.checked.indexOf(elem.device) >= 0).
-        map((elem, index) =>
-            <tr key={elem.id}>
-              <th scope="row">
-                {index + 1}
-              </th>
-              <td>
-                {elem.name}
-              </td>
-              <td>
-                <Rating step={1} fractions={10} initialRate={elem.rating}
-                        readonly/>
-              </td>
-              <td>
-                {elem.price}
-              </td>
-            </tr>,
-        );
+    return list.filter(elem => this.props.checked.indexOf(elem.device) >= 0).map((elem, index) =>
+      <tr key={elem.id}>
+        <th scope="row">
+          {index + 1}
+        </th>
+        <td>
+          {elem.name}
+        </td>
+        <td>
+          <Rating step={1} fractions={10} initialRate={elem.rating} readonly />
+        </td>
+        <td>
+          {elem.price}
+        </td>
+      </tr>
+    );
   }
 
   render() {
     return (
-        <Table responsive>
-          <thead>
+      <Table responsive>
+        <thead>
           <tr>
             <th>#</th>
             <th onClick={this.onSortChange.bind(this, 'name')}>Product Name</th>
             <th onClick={this.onSortChange.bind(this, 'rating')}>Rating</th>
             <th onClick={this.onSortChange.bind(this, 'price')}>Price</th>
           </tr>
-          </thead>
-          <tbody>
-          {this.tableRender(this.props.price_list)}
-          </tbody>
-        </Table>
+        </thead>
+        <tbody>
+          {this.tableRender(this.state.list)}
+        </tbody>
+      </Table>
     );
   }
 }
 
 SortableTable.propTypes = {
   price_list: PropTypes.arrayOf(PropTypes.object).isRequired,
-  checked: PropTypes.arrayOf(PropTypes.string).isRequired,
+  checked: PropTypes.arrayOf(PropTypes.string).isRequired
 };
 
 export default SortableTable;
